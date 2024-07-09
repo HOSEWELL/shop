b4c5649b2416ab324505a804847152ddd9731cae
HOSEWELL <hosewellkaranja@gmail.com>
2024-07-09 09:34:06 +0300

    modifications

--- src/components/product-profile.jsx
+++ src/components/product-profile.jsx
@@ -11,16 +11,19 @@ const ProductProfile = () => {
     return (
         <>
             <Navbar />
-            <div class="position-absolute" style={{borderRadius:"30px",padding:"12px", justifyContent:"center",margin:"3em 10em", width:"60%"}} >
-                <img src={product.images[0]} className="card-img-top" alt="card img cap" style={{borderRadius:"30px"}}/>
+            <div className="position-absolute" style={{borderRadius:"30px",padding:"12px", justifyContent:"center",margin:"3em 10em", width:"60%",display:"flex", gap:"20px"}} >
+
+                <div><img src={product.images[0]} className="card-img-top" alt="card img cap" style={{borderRadius:"30px", height:"40em", width:"40em"}}/></div>
 
                 <div className="card-body">
                     <h5 className="card-title">{product.title}</h5>
                     <h5 className="card-title">$ {product.price}</h5>
                     <p className="card-text">{product.description} </p>
 
-                    <Link to="/" state={product} className="btn btn-primary" style={{marginRight:"10em"}}>Home</Link>
                     <Link to="/buy" className="btn btn-primary">Buy</Link>
+                    <br />
+                    <Link to="/" state={product} className="btn btn-primary" style={{marginRight:"10em"}}>Home</Link>
+                   
                     
                 </div>
             </div>
